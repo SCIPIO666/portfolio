4ed590097cfe94c949bd06de7cdfd9945f11188e
SCIPIO666 <eaphoney@gmail.com>
2026-07-22 16:18:05 +0300

    fix: lenis

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import './App.css';
 import DevScipioAnimation from './components/DevScipioAnimation';
 import SinglePage from './pages/SinglePage';
 import Navbar from './components/Navbar';
-import useLenis from './hooks/useLenis';
 function App() {
   const [loading, setLoading] = useState(true);
-  useLenis();
 
   useEffect(() => {
     const t = setTimeout(() => setLoading(false), 5000);
